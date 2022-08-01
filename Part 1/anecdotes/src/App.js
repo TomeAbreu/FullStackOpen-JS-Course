f8fe85d90b8a99bf45e0024347e64d0b6db0bb10
Tome Abreu <tomeabreu05@gmail.com>
2022-08-01 12:22:29 +0100

    anecdotes application finished

--- Part 1/anecdotes/src/App.js
+++ Part 1/anecdotes/src/App.js
@@ -1,5 +1,9 @@
 import { useState } from "react";
 
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
 const App = () => {
   const anecdotes = [
     "If it hurts, do it more often.",
@@ -12,8 +16,63 @@ const App = () => {
   ];
 
   const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
+  const [max, setMax] = useState(0);
+  const [maxSelect, setMaxSelect] = useState(0);
 
-  return <div>{anecdotes[selected]}</div>;
+  //Reload Quote
+  const reloadQuote = (anecdotes) => {
+    //Increse feedback reaction
+    let randomIndex = Math.floor(Math.random() * anecdotes.length - 1) + 1;
+    setSelected(randomIndex);
+  };
+
+  //Remember that the correct way of updating state stored
+  //in complex data structures like objects and arrays is to make
+  // a COPY of the state.
+  const increaseVote = (votes, selected) => {
+    const copyVotes = [...votes];
+    copyVotes[selected] += 1;
+    setVotes(copyVotes);
+
+    //Check if most voted quote needs to be changed
+    checkMaxQuote(copyVotes);
+  };
+
+  const checkMaxQuote = (copyVotes) => {
+    const maxVote = Math.max(...copyVotes);
+    const maxIndex = copyVotes.indexOf(max);
+
+    setMax(maxVote);
+    setMaxSelect(maxIndex);
+  };
+
+  return (
+    <div>
+      <div>
+        <b>Anecdote of the Day</b>
+        <div>{anecdotes[selected]}</div>
+        <div>
+          Has
+          {" " + votes[selected] + " votes."}
+        </div>
+        <Button handleClick={() => increaseVote(votes, selected)} text="Vote" />
+        <Button
+          handleClick={() => reloadQuote(anecdotes, selected)}
+          text="Next anecdote"
+        />
+      </div>
+      <p></p>
+      <div>
+        <b>Anecdote with most votes</b>
+        <div>{anecdotes[maxSelect]}</div>
+        <div>
+          Has
+          {" " + votes[maxSelect] + " votes."}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
