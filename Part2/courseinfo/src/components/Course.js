5b85ce85a95f141c44f6bc606b3fb7caade5e3a9
Tome Abreu <tomeabreu05@gmail.com>
2022-08-02 17:45:30 +0100

    Exercise 2.2 completed

--- Part2/courseinfo/src/components/Course.js
+++ Part2/courseinfo/src/components/Course.js
@@ -3,11 +3,16 @@ import Header from "./Header";
 import Content from "./Content";
 
 const Course = ({ course }) => {
-  console.log("Course", course);
+  //REDUCE FUNCTION: High order function that has a callback function that calculates sum of exercices in parts array
+  const exercicesSum = course.parts.reduce((sum, part) => {
+    return sum + part.exercises;
+  }, 0);
+
   return (
     <div>
       <Header text={course.name} />
       <Content parts={course.parts} />
+      <b>Total of {exercicesSum} exercices</b>
     </div>
   );
 };
