74266c35601c50288f0ba4b14b91e29946df1b65
Tome Abreu <tomeabreu05@gmail.com>
2022-08-05 16:03:12 +0100

    Exercise 2.8 Completed

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -2,23 +2,31 @@ import { useState } from "react";
 import Person from "./components/Person";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1234567" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleNewNameChange = (event) => {
     setNewName(event.target.value);
   };
 
+  const handleNewNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
   const addNewPerson = (event) => {
     event.preventDefault();
-    const personObject = { name: newName };
+    const personObject = { name: newName, number: newNumber };
 
     //Check if person name already exists before update state variable persons
     if (checkDuplicatePersonName() === false) {
       //Update State variables
       setPersons(persons.concat(personObject));
-      setNewName("");
     }
+    setNewName("");
+    setNewNumber("");
   };
 
   const checkDuplicatePersonName = () => {
@@ -40,13 +48,16 @@ const App = () => {
           name: <input onChange={handleNewNameChange} value={newName} />
         </div>
         <div>
+          number: <input onChange={handleNewNumberChange} value={newNumber} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map((person) => (
-          <Person key={person.name} name={person.name}></Person>
+          <Person key={person.name} person={person}></Person>
         ))}
       </ul>
     </div>
