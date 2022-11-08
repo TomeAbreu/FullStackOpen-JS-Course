5c870534449bd79fcb642649be7ae5a3cbef4790
Tome Abreu <tomeabreu05@gmail.com>
2022-11-08 22:40:45 +0000

    Part5: Exercise 5.6 and 5.7 Completed

--- Part5/bloglist-frontend/src/components/Blog.js
+++ Part5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,33 @@
 import React from "react";
+import { useState } from "react";
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+const Blog = ({ blog }) => {
+  const [blogDetailsVisible, setBlogDetailsVisible] = useState(false);
+
+  const toggleBlogDetails = () => {
+    if (!blogDetailsVisible) {
+      console.log("Show blog details");
+    }
+    setBlogDetailsVisible(!blogDetailsVisible);
+  };
+
+  const showBlogDetails = () => (
+    <div>
+      <p>{blog.url}</p>
+      <p>{blog.likes}</p>
+      <p>{blog.author}</p>
+    </div>
+  );
+
+  return (
+    <div className="blog">
+      <span>{blog.title} </span>
+      <button onClick={toggleBlogDetails}>
+        {blogDetailsVisible ? "hide" : "view"}
+      </button>
+      {blogDetailsVisible === true && showBlogDetails()}
+    </div>
+  );
+};
 
 export default Blog;
