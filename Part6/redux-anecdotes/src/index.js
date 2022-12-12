c2e415ac0d6ebb85ade1b9a6d0db301c3c52021b
Tome Abreu <tomeabreu05@gmail.com>
2022-12-12 16:32:51 +0000

    Exercises 6.3  to 6.9

--- Part6/redux-anecdotes/src/index.js
+++ Part6/redux-anecdotes/src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
-import reducer from './reducers/anecdoteReducer'
+import anecdotesReducer from './reducers/anecdoteReducer'
 
-const store = createStore(reducer)
+//Store that stores the state and uses the reducer anecdoteReducer to handle actions
+//to change the state
+const store = createStore(anecdotesReducer)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
--- Part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ Part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -4,26 +4,63 @@ const anecdotesAtStart = [
   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
   'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
 ]
 
-const getId = () => (100000 * Math.random()).toFixed(0)
-
+const generateId = () => Number((Math.random() * 1000000).toFixed(0))
 const asObject = (anecdote) => {
   return {
     content: anecdote,
-    id: getId(),
-    votes: 0
+    id: generateId(),
+    votes: 0,
   }
 }
 
+//Initial state in the application
 const initialState = anecdotesAtStart.map(asObject)
 
-const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
-
-  return state
+//Reducer uses actions  and a state to change to a new state
+//Actions are used to change the state in a redux store
+//state=initialState tells reducer the default state in the store
+const anecdoteReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'NEW_ANECDOTE':
+      return [...state, action.data]
+    case 'INCREASE_VOTE':
+      const id = action.data.id
+      const anecdoteToIncreaseVote = state.find((n) => n.id === id)
+      const increasedAnecdote = {
+        ...anecdoteToIncreaseVote,
+        votes: anecdoteToIncreaseVote.votes + 1,
+      }
+      return state.map((note) => (note.id !== id ? note : increasedAnecdote))
+    default:
+      return state
+  }
 }
 
-export default reducer
+//Functions that create actions are called action creators
+
+//Action creator createNote
+export const createNote = (content) => {
+  return {
+    type: 'NEW_ANECDOTE',
+    data: {
+      content,
+      votes: 0,
+      id: generateId(),
+    },
+  }
+}
+
+//Action creator increaseVote
+export const increaseVote = (id) => {
+  return {
+    type: 'INCREASE_VOTE',
+    data: { id },
+  }
+}
+
+//A module can have only one default export, but multiple "normal" exports
+
+export default anecdoteReducer
