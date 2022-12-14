ac9e0df2b80cabcbfb724df534eef832d3ac2707
Tome Abreu <tomeabreu05@gmail.com>
2022-12-14 16:54:01 +0000

    Ex 6.10 Change Reducer to use createSlice Redux

--- Part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ Part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,5 +1,17 @@
-const notificationReducer = (state = 'Notification content', action) => {
-  return state
-}
+import { createSlice } from '@reduxjs/toolkit'
 
-export default notificationReducer
+const notificationSlice = createSlice({
+  //Define type parameter showNotification will have: notification/showNotificiation
+  name: 'notification',
+  //Initial State of the Reducer
+  initialState: 'Notification content',
+  //Reducers Functions that handle state
+  reducers: {
+    showNotification(state, action) {
+      return state
+    },
+  },
+})
+
+export const { showNotification } = notificationSlice.actions
+export default notificationSlice.reducer
