59459474faa3f3ee882b80b936dd3d96cee667e8
Tome Abreu <tomeabreu05@gmail.com>
2022-12-16 16:05:23 +0000

    Ex6.19 Refactor Notification - ConnectNotification

--- Part6/redux-anecdotes/src/components/Notification.js
+++ Part6/redux-anecdotes/src/components/Notification.js
@@ -1,19 +1,27 @@
-import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-  const notification = useSelector((state) => state.notification)
-
+const Notification = (props) => {
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1,
   }
 
-  if (!notification) {
+  if (!props.notification) {
     return <div></div>
   } else {
-    return <div style={style}>{notification}</div>
+    return <div style={style}>{props.notification}</div>
   }
 }
+//MapStateToProps connect functionn to map the state to props of Notification component
+//In this case Notification component used notification state with useSelector hook
+//Now it's in the props of ConnectedNotification component
+const mapStateToProps = (state) => {
+  return {
+    notification: state.notification,
+  }
+}
+//Connected Notifcation Component
+const ConnectedNotification = connect(mapStateToProps)(Notification)
 
-export default Notification
+export default ConnectedNotification
--- Part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ Part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -25,6 +25,7 @@ export const { showNotification } = notificationSlice.actions
 // //Async Action creator to showNotification and update state of notification
 // using Redux Thunk library
 export const showNotificationAction = (message, awaitTime) => {
+  console.log('OLA')
   return async (dispatch) => {
     dispatch(showNotification(message))
     await setNotificationTimer(awaitTime * 1000)
