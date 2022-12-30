d873f3b75ab3dd0d4af47b0764329870c9434685
Tome Abreu <tomeabreu05@gmail.com>
2022-12-30 11:24:32 +0000

    WebPack: Initialize and bundle react app

---
+++ Part7/webpackByHand/webpack.config.js
@@ -0,0 +1,26 @@
+//We define the functionality of webpack in this file, which we initialize it
+
+const path = require('path')
+
+const config = {
+  //File that will be the entry point of our app to be bundle
+  entry: './src/index.js',
+  //Location where the bundle code will be located
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'main.js',
+  },
+  //Define loader to convert JSX code to JavaScript
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react'],
+        },
+      },
+    ],
+  },
+}
+module.exports = config
