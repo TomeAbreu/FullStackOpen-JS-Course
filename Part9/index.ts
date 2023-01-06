f88141661c9eb0d2ad9367fda28e334e12db1daf
Tome Abreu <tomeabreu05@gmail.com>
2023-01-06 15:34:47 +0000

    Install and configure express server

--- Part9/index.ts
+++ Part9/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello world!')
+import express from 'express'
+
+const app = express()
+
+app.get('/ping', (_req, res) => {
+  res.send('pong')
+})
+
+const PORT = 3003
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
