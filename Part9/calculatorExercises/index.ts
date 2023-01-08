f426f3ac7ba7264af2c2a1f5515eb9fa3f4d99c4
Tome Abreu <tomeabreu05@gmail.com>
2023-01-08 18:20:17 +0000

    Structure Repository

--- Part9/calculatorExercises/index.ts
+++ Part9/calculatorExercises/index.ts
@@ -1,15 +1,15 @@
-import express from 'express'
-import { bmiCalculator } from './bmiCalculator'
+import express from 'express';
+import { bmiCalculator } from './bmiCalculator';
 
-const app = express()
+const app = express();
 
 app.get('/hello', (_req, res) => {
-  res.send('Hello Full Stack!')
-})
+  res.send('Hello Full Stack!');
+});
 
 app.get('/bmi', (req, res) => {
-  const requestQuery = req.query
-  let response
+  const requestQuery = req.query;
+  let response;
 
   if (requestQuery.height && requestQuery.weight) {
     response = {
@@ -19,18 +19,18 @@ app.get('/bmi', (req, res) => {
         Number(requestQuery.height),
         Number(requestQuery.weight)
       ),
-    }
-    return res.json(response).status(200)
+    };
+    return res.json(response).status(200);
   } else {
     response = {
       error: 'malformatted parameters',
-    }
-    return res.json(response).status(408)
+    };
+    return res.json(response).status(408);
   }
-})
+});
 
-const PORT = 3003
+const PORT = 3003;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
