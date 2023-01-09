f3ef34206ef39413e624a2748b637f5976f16785
Tome Abreu <tomeabreu05@gmail.com>
2023-01-09 18:04:01 +0000

    Adding type for post request and refactor types

--- Part9/Ilari/src/routes/diaries.ts
+++ Part9/Ilari/src/routes/diaries.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import diaryService from '../services/diaryService';
+import toNewDiaryEntry from '../utils';
 
 const router = express.Router();
 
@@ -17,8 +18,19 @@ router.get('/:id', (req, res) => {
   }
 });
 
-router.post('/', (_req, res) => {
-  res.send('Saving a diary!');
+router.post('/', (req, res) => {
+  try {
+    const newDiaryEntry = toNewDiaryEntry(req.body);
+
+    const addedEntry = diaryService.addDiary(newDiaryEntry);
+    res.json(addedEntry);
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong.';
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    res.status(400).send(errorMessage);
+  }
 });
 
 export default router;
