a87a2755bb2ef55ab6d446d07388c1ea32ebc668
Tome Abreu <tomeabreu05@gmail.com>
2023-01-09 15:19:42 +0000

    Part9:Add Patentor front-end and link it to bakend

--- Part9/Patientor-backend/index.ts
+++ Part9/Patientor-backend/index.ts
@@ -1,5 +1,14 @@
 import express from 'express';
+import cors from 'cors';
+
 const app = express();
+
+//Middleware to use static build
+app.use(express.static('build'));
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
+
 app.use(express.json());
 
 const PORT = 3000;
